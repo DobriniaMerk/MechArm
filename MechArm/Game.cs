e0b02b0e15501bb82c4d53aeb829d0a34b94d32b
DobriniaMerk <Dobrinia.merk@yandex.ru>
2021-11-12 19:07:43 +0300

    aedded multiple arm support

--- MechArm/Game.cs
+++ MechArm/Game.cs
@@ -11,20 +11,25 @@ namespace MechArm
 {
     class Game
     {
-        Arm arm = new Arm();
+        //Arm arm = new Arm();
+        List<Arm> arms = new List<Arm>();
+        int i = 0;
         bool pause = true;
 
         public void MousePressed(object sender, MouseButtonEventArgs e)
         {
             if (pause)
                 if (e.Button == Mouse.Button.Left)
-                    arm.AddPoint(new Vector2f(e.X, e.Y));
+                    if (arms.Count == 0)
+                        arms.Add(new Arm());
+                    arms[i].AddPoint(new Vector2f(e.X, e.Y));
         }
 
         public void MouseMoved(object sender, MouseMoveEventArgs e, RenderWindow rw)
         {
             if (!pause)
-                arm.Seek((Vector2f)Mouse.GetPosition(rw));
+                foreach(Arm arm in arms)
+                    arm.Seek((Vector2f)Mouse.GetPosition(rw));
         }
 
         public void KeyPressed(object sender, KeyEventArgs e, RenderWindow rw)
@@ -35,14 +40,20 @@ namespace MechArm
                     pause = !pause;
                     break;
                 case Keyboard.Key.R:
-                    arm = new Arm();
+                    arms = new List<Arm>();
+                    i = 0;
+                    break;
+                case Keyboard.Key.N:
+                    arms.Add(new Arm());
+                    i++;
                     break;
             }
         }
 
         public void Draw(RenderWindow rw)
         {
-            arm.Draw(rw);
+            foreach(Arm arm in arms)
+                arm.Draw(rw);
         }
     }
 }
